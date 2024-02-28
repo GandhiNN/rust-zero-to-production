8a3952b6dad7992ecc14c5c58e535bc4fbcbec68
Ngakan Gandhi <n.nyoman.gandhi@gmail.com>
2024-02-28 14:35:20 +0700

    update logic

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,8 @@ use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    run().await
+    let listener = std::net::TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to port 8000");
+    // Bubble up the io::Error if we failed to bind the address
+    // Otherwise call .await on our Server
+    run(listener)?.await
 }
