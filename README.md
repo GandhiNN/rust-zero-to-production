fe399f22ec2dca2166681bd9353048bf1c46d650
Ngakan Gandhi <n.nyoman.gandhi@gmail.com>
2024-10-18 15:04:40 +0700

    rename and refactor

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# zero2prod
+# Rust Zero-to-Production
 This is a tutorial repository for backend development in Rust.
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use zero2prod::run;
+use std::net::TcpListener;
+
+use zero_to_production::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = std::net::TcpListener::bind("127.0.0.1:8000").expect("Failed to bind to port 8000");
-    // Bubble up the io::Error if we failed to bind the address
-    // Otherwise call .await on our Server
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind port");
     run(listener)?.await
 }
